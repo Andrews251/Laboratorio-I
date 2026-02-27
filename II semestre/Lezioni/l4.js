8796370fbe9d561cc4f2b4bfade3ab28c5484c7e
Andrea Molino <andrea.molino01@gmail.com>
2026-02-27 12:04:48 +0100

    commit modifica

---
+++ II semestre/Lezioni/l4.js
@@ -0,0 +1,19 @@
+//moduli, require, import/export
+
+//esempio: const mod = require ('./modulo');
+
+//import * as mod from './modulo.js'
+//export function f2()
+//guardati le diverse keyword relative a import ed export
+
+//classi Wrapper: Number, Boolean, String etc. -> iniziano tutti con maiuscola
+// diverso da number, string, boolean per il controllo del tipo
+
+//guardati la slide tu RegExp
+//array con tipi macchina: Int8Array, Int16Array, etc.
+
+//funzioni per gli insiemi
+let s = new Set()
+
+//mappe
+//JSON, guardatelo per scambiare o salvare dati, guarda la sintassi
